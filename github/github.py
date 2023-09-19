620f5bfa2fb36ce28cad7d5983a9c5e1802bbcad
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-09-19 12:08:23 +0300

    Updating

--- github/github.py
+++ github/github.py
@@ -19,38 +19,85 @@ class Github():
         self.driver.get(self.url)
         self.driver.maximize_window()
 
+    def wait(self,sec=2):
+        time.sleep(sec)
+
     def signIn(self):
         login=self.driver.find_element(By.CSS_SELECTOR,'[href="/login"]') 
         login.click()
-        self.driver.implicitly_wait(2)
+        
+        # time.sleep(2)
+        self.wait()
 
         self.driver.find_element(By.NAME, "login").send_keys(self.username)
         self.driver.find_element(By.NAME, "password").send_keys(self.password)
         
         self.driver.find_element(By.NAME, "commit").send_keys(Keys.ENTER)
 
-        time.sleep(2)
+       
+        self.wait()
         self.driver.get(self.url+self.username)
-        time.sleep(5)
+        
+        self.wait()
 
-    def getRepos(self):
+    def loadRepos(self):
         self.driver.get(self.url+self.username)
-        time.sleep(2)
+        
+        self.wait()
+
         repos=self.driver.find_element(By.CSS_SELECTOR,'[data-tab-item="repositories"]')
         repos.click()
-        time.sleep(2)
+    
 
-        repos_list=self.driver.find_element(By.ID, "user-repositories-list").find_element(By.TAG_NAME, 'ul').find_elements(By.TAG_NAME, 'li')
+        while True:
+
+            self.wait()
+
+            repos_list=self.driver.find_element(By.ID, "user-repositories-list").find_element(By.TAG_NAME, 'ul').find_elements(By.TAG_NAME, 'li')
+
+            self.count_repos+=len(repos_list)
+
+            if self.count_repos > 0:
+                for rep in repos_list:
+                    repo_title=rep.find_element(By.CSS_SELECTOR, '[itemprop="name codeRepository"]')
+                    self.repositories.append(repo_title.text)
+
+
+            try:
+
+                self.wait()
+
+                next_button=self.driver.find_element(By.CLASS_NAME, "next_page")
+
+                if next_button.is_displayed():
+                    #print("Next button exist")
+
+                    #print(next_button.get_attribute("class").split(" "))
+
+                    if "disabled" not in next_button.get_attribute("class").split(" "):
+                        next_button.click()
+                       
+                    else:
+                        #print("Next Button is disabled now")
+                        break
+
+            except Exception:
+                #print("Next Button does not exist")
+                break
+                
         
-        self.count_repos+=len(repos_list)
 
-        if len(repos_list) >0:
-            for rep in repos_list:
-                repo_title=rep.find_element(By.CSS_SELECTOR, '[itemprop="name codeRepository"]')
-                self.repositories.append(repo_title.text)
+    def getRepos(self):
+
+        self.loadRepos()
 
         return self.repositories
 
+
     def getReposCount(self):
+
+        if self.count_repos==0:
+            self.loadRepos()
+
         return self.count_repos
 
